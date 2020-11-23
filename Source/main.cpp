e963148811a031955a8fa4305385c3d0b5c8a58d
Oliver Smith <olie.smit0@gmail.com>
2020-11-23 18:06:36 +0000

    Engine updates.

--- Source/main.cpp
+++ Source/main.cpp
@@ -74,9 +74,6 @@ int main()
     Controller* controller1 = new Controller(GLFW_JOYSTICK_1);
 
     Scene testScene("Assets//Materials");
-    testScene.setOctreeMaxDivisions(2, AccelerationStructure::Static);
-    testScene.setOctreeMaxDivisions(1, AccelerationStructure::Dynamic);
-    testScene.setOctreeMaxDivisions(3, AccelerationStructure::Physics);
 
     std::array<std::string, 6> skybox{ "./Assets/Textures/bluecloud_ft.jpg",
                                        "./Assets/Textures/bluecloud_bk.jpg",
@@ -150,10 +147,7 @@ int main()
             frameStartTime = currentTime;
             scriptEngine.tick(frameDelta);
 
-            // These update the scene in the engine so need to be guarded with a mutex.
             player1->update(controller1, eng);
-
-            testScene.computeBounds(AccelerationStructure::Dynamic);
 
             std::vector<Scene::Intersection> collisions = testScene.getIntersections(player1Instance);
             for(const auto& collision : collisions)
